ae262fcf3c9661a8b1a548c40fd3609132933cc5
Holychikenz <maskins@lbl.gov>
2023-02-01 15:32:58 -0800

    eggs

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import os
 from setuptools import setup
 
+VERSION = "0.0.1"
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
         name = "idlescape",
+        version = VERSION,
         packages=['idlescape'],
 )
