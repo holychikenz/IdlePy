b9fa215ca32294cfd1d6fa0e5d63502e4cb2ad41
Holychikenz <maskins@lbl.gov>
2023-02-01 15:35:21 -0800

    hhweeel

--- panel_convert.sh
+++ panel_convert.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-panel convert ZonePanel.py --to pyodide-worker --out docs/zones --requirements "git+https://github.com/holychikenz/IdlePy#egg=idlescape" hvplot
+#panel convert ZonePanel.py --to pyodide-worker --out docs/zones --requirements "git+https://github.com/holychikenz/IdlePy#egg=idlescape" hvplot
+panel convert ZonePanel.py --to pyodide-worker --out docs/zones --requirements "https://github.com/holychikenz/IdlePy/dist/idlescape-0.0.1-py3-none-any.whl" hvplot
+#panel convert ZonePanel.py --to pyodide-worker --out docs/zones --requirements requirements.txt
