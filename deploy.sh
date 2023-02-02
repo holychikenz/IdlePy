ed67742073afe4fd4f50d398598f443c4637d5cd
Holychikenz <maskins@lbl.gov>
2023-02-02 09:19:29 -0800

    will this deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python setup.py bdist_wheel
-url="/IdlePy/dist/idlescape-0.0.2-py3-none-any.whl"
+url="/IdlePy/dist/idlescape-0.0.2-cp310-cp310-linux_x86_64.whl"
 panel convert ZonePanel.py --to pyodide-worker --out docs/zones --requirements $url hvplot
