1a94d95ced29145923a0a000f30ebae706330a17
Holychikenz <maskins@lbl.gov>
2023-02-01 18:45:43 -0800

    no numba

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python setup.py bdist_wheel
 url="/IdlePy/dist/idlescape-0.0.2-py3-none-any.whl"
-panel convert ZonePanel.py --to pyodide-worker --out docs/zones --requirements $url hvplot numba
+panel convert ZonePanel.py --to pyodide-worker --out docs/zones --requirements $url hvplot
