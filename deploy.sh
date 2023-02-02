72523f1e6338e31829a635f10c52bc86a3d4d9a2
Holychikenz <maskins@lbl.gov>
2023-02-02 09:29:41 -0800

    wasm

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python setup.py bdist_wheel
-url="/IdlePy/dist/idlescape-0.0.2-cp310-cp310-linux_x86_64.whl"
+pyodide build
+url="/IdlePy/dist/idlescape-0.0.2-cp310-cp310-emscripten_3_1_27_wasm32.whl"
 panel convert ZonePanel.py --to pyodide-worker --out docs/zones --requirements $url hvplot
